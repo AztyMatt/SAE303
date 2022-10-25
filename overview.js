4b53e945c705efecf0bc1267c23ee5464f06adbd
AztyUwU <benderpro102@gmail.com>
2022-10-25 17:23:01 +0200

    dernieres modifs

--- overview.js
+++ overview.js
@@ -1,46 +1,44 @@
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(Coupons);
-google.charts.setOnLoadCallback(Livraisons);
+google.charts.setOnLoadCallback(function() {
+    Piechart("coupons","Coupons utilisés :","");
+    Piechart("livraisons","Modes de livraisons utilisés :","");
+    Piechart("reglements","Moyens de paiements utilisés :","");
+    Piechart("mois","CA par mois :","true");
+    Piechart("produits","CA par produits :","true");
+    Piechart("regions","CA par régions :","true");
+});
 
-function Coupons() {
+function Piechart() {
 
     var jsonData = $.ajax({
-            url: "./ressources/json/coupons.json",
+            url: "./ressources/json/"+arguments[0]+".json",
             dataType: "json",
             async: false,
             }).responseText;
-            
+
     var data = new google.visualization.DataTable();
     data.addColumn({type:'string', id:'Utilisation de coupon', label: 'Utilisation de coupon'});
     data.addColumn({type:'number', id:'Nombre de coupons', label: 'Nombre de coupons'});
     data.addRows($.parseJSON(jsonData));
 
+    if (arguments[2]){
+        var formatNumber = new google.visualization.NumberFormat({
+            pattern: '###,### €'
+            });
+            formatNumber.format(data, 1);
+    }
+
     var options = {
-    title: 'Utilisation des coupons :',
-    colors:['#f84f31','#23c552']
+        title: arguments[1],
+        titleTextStyle: { color: 'white', fontSize: 16, bold: 'true'},
+        slices: {  0: {color: '#dc3912'}, 1: {color: '#3366cc'}},
+        chartArea: {width: 500, height: 300, left: 30, top: 50},
+        backgroundColor: 'transparent',
+        legend: {position: 'right', textStyle: { color: 'white'}},
+        pieSliceBorderColor: '#1f1f1f',
     };
 
-    var chart = new google.visualization.PieChart(document.getElementById('coupons'));
+    var chart = new google.visualization.PieChart(document.getElementById(arguments[0]));
     chart.draw(data, options);
 }
 
-function Livraisons() {
-
-    var jsonData = $.ajax({
-            url: "./ressources/json/livraisons.json",
-            dataType: "json",
-            async: false,
-            }).responseText;
-            
-    var data = new google.visualization.DataTable();
-    data.addColumn({type:'string', id:'Utilisation de coupon', label: 'Utilisation de coupon'});
-    data.addColumn({type:'number', id:'Nombre de coupons', label: 'Nombre de coupons'});
-    data.addRows($.parseJSON(jsonData));
-
-    var options = {
-    title: 'Mode de livraisons utilisés :'
-    };
-
-    var chart = new google.visualization.PieChart(document.getElementById('livraisons'));
-    chart.draw(data, options);
-}

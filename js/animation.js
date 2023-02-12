3a24575e6c9be41974e6c942e98228ba890b3afa
AztyUwU <benderpro102@gmail.com>
2023-02-11 23:28:45 -0500

    animations

--- js/animation.js
+++ js/animation.js
@@ -1,15 +1,12 @@
 window.addEventListener('load', () => {
-
-
-    //Accueil
     if (document.querySelector('body') !== null){
         const TL = gsap.timeline({paused: true});
     
     TL
     .from("header", 1, {autoAlpha:0, ease:"ease-out"},'start')
     .from(".description", 1, {autoAlpha:0, transform: "translateX(500px)", ease:"ease-out"},'1')
-    .from(".charts", 1.5, {autoAlpha:0, ease:"ease-out"},'1')
-    .staggerFrom('.piechart', 0.5, {autoAlpha:0, pointerEvents: "none", ease: "linear"}, 0.5, '1')
+    .from(".charts", 1.5, {autoAlpha:0, ease:"ease-out"},'2')
+    .staggerFrom('.piechart', 0.5, {autoAlpha:0, pointerEvents: "none", ease: "linear"}, 0.5, '2')
         TL.play();
     }
 });
